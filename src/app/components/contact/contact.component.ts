02f7728b9cf86047d7dacb5ea783819b63f6a336
Seif BS <seifbensghaier@gmail.com>
2022-02-10 15:21:15 +0100

    added contact email and copy to clipboard onclick

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
+  public isCopied: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  copyEmail() {
+    var copyText = document.getElementById('myEmail') as HTMLInputElement;
+
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    this.isCopied = true;
   }
-
 }
